f1c62b0215cced1bb178c02c51254a8468b5f56b
Elimihele God's favour <godsfavour1975@gmail.com>
2021-10-22 15:36:28 +0100

    Update Index.js

--- js/Index.js
+++ js/Index.js
@@ -191,6 +191,9 @@ function runSpeechRecognition() {
                 // This runs when the speech recognition service returns result
                 recognition.onresult = function(event) {
                     var transcript = event.results[0][0].transcript;
+var confidence = event.results[0][0].confidence;
+                    output.innerHTML = "<b>Text:</b> " + transcript ;
+                   
                      output.classList.remove("hide");
                                         if (transcript == "play" || transcript == "play song") {
                      return playSong();
